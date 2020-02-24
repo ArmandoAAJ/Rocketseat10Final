90320d432d6b62d94ec0119cd21782a54ee9bcd5
Armando Joergensen <armando-a-j@hotmail.com>
2020-02-24 13:48:24 -0300

    Cors

--- api-node/src/app.js
+++ api-node/src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import path from 'path';
+import Youch from 'youch';
+import Cors from 'cors';
+import 'express-async-errors';
 import routes from './routes';
-
 import './database';
 
 class App {
@@ -9,15 +12,31 @@ class App {
 
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(Cors());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
     this.server.use(routes);
   }
-}
 
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
+      }
+      return res.status(500).json({ error: 'Internal server error' });
+    });
+  }
+}
+// SEM O SUCRASE USAMOS module.exports = App().server;
 export default new App().server;
