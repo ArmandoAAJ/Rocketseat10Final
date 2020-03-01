a541be044251274389b7bb8602145207f78edc7b
Armando Joergensen <armando-a-j@hotmail.com>
2020-03-01 14:34:36 -0300

    Editar Encomenda
    Adicionando Rota de  editar encomanda

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -4,14 +4,16 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 import SigIn from '~/pages/SignIn';
 import Dashboard from '~/pages/Dashboard';
-import NewDeliveryman from '~/pages/Dashboard/New';
+import NewOrder from '~/pages/Dashboard/New';
+import EditOrder from '~/pages/Dashboard/Edit';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SigIn} />
       <Route path="/dashboard" component={Dashboard} isPrivate />
-      <Route path="/novaencomenda" component={NewDeliveryman} isPrivate />
+      <Route path="/novaencomenda" component={NewOrder} isPrivate />
+      <Route path="/editarencomenda/:id" component={EditOrder} isPrivate />
     </Switch>
   );
 }
