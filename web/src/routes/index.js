7254d298e79a8a47055c76da6891591192649da8
Armando Joergensen <armando-a-j@hotmail.com>
2020-03-03 00:34:39 -0300

    Deliveryman
    Adicioanndo rotas para cadastro de entregadores

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -6,6 +6,9 @@ import SigIn from '~/pages/SignIn';
 import Dashboard from '~/pages/Dashboard';
 import NewOrder from '~/pages/Dashboard/New';
 import EditOrder from '~/pages/Dashboard/Edit';
+import Deliveryman from '~/pages/Deliveryman';
+import NewDeliveryman from '~/pages/Deliveryman/New';
+import EditDeliveryman from '~/pages/Deliveryman/Edit';
 
 export default function Routes() {
   return (
@@ -14,6 +17,13 @@ export default function Routes() {
       <Route path="/dashboard" component={Dashboard} isPrivate />
       <Route path="/novaencomenda" component={NewOrder} isPrivate />
       <Route path="/editarencomenda/:id" component={EditOrder} isPrivate />
+      <Route path="/entregador" component={Deliveryman} isPrivate />
+      <Route path="/novoentregador" component={NewDeliveryman} isPrivate />
+      <Route
+        path="/editarentregador/:id"
+        component={EditDeliveryman}
+        isPrivate
+      />
     </Switch>
   );
 }
