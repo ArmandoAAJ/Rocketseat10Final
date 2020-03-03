16d787b132464164a93ecb6221eaa091741d9d43
Armando Joergensen <armando-a-j@hotmail.com>
2020-03-03 16:07:26 -0300

    Deliveryman Index
    Criação e ajustes na busca e exibição dos deliverymans

--- web/src/pages/Deliveryman/index.js
+++ web/src/pages/Deliveryman/index.js
@@ -9,7 +9,10 @@ import Avatar from 'react-avatar';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import queryString from 'query-string';
+import { confirmAlert } from 'react-confirm-alert';
+import { toast } from 'react-toastify';
 import api from '~/services/api';
+import history from '~/services/history';
 import { Container, Content, ContentTable } from './styles';
 import Header from '~/components/Header';
 
@@ -38,16 +41,47 @@ export default function Dashboard({ location }) {
     loadData();
   }, [q]);
 
+  function handleSubmit(data) {
+    history.push(`entregador?q=${data.q}`);
+  }
+
+  async function handleDeliverymanDelete(id) {
+    try {
+      await api.delete(`deliverymans/${id}`);
+      toast.success('Entregador deletado com sucesso');
+      history.push(`entregador?q=`);
+    } catch (err) {
+      toast.error('Entregador não encontrado!');
+    }
+  }
+
+  function confirmDeliverymanDelete(id) {
+    confirmAlert({
+      title: 'Deletar Entregador',
+      message: 'Você está certo disso? Não poderá desfazer está ação!',
+      buttons: [
+        {
+          label: 'Sim',
+          onClick: () => handleDeliverymanDelete(id),
+        },
+        {
+          label: 'Não',
+          onClick: () => {},
+        },
+      ],
+    });
+  }
+
   return (
     <Container>
       <Header />
       <Content>
         <nav>
-          <h2>Gerenciando Encomendas</h2>
+          <h2>Gerenciando Entregadores</h2>
         </nav>
         <div>
-          <Form onSubmit="">
-            <Input name="" placeholder="Buscar Encomendas" />
+          <Form onSubmit={handleSubmit}>
+            <Input name="q" placeholder="Buscar Entregadores" />
           </Form>
           <Link to="/novoentregador">
             <button className="cadastrar">
@@ -57,62 +91,64 @@ export default function Dashboard({ location }) {
           </Link>
         </div>
         <ContentTable>
-          {/* {orders.length > 0 ? ( */}
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Foto</th>
-                <th>Nome</th>
-                <th>Email</th>
-                <th>Ações</th>
-              </tr>
-            </thead>
-            <tbody>
-              {deliverymans.map(deliveryman => (
-                <tr key={deliveryman.id}>
-                  <td>#{deliveryman.id}</td>
-                  <td>
-                    <Avatar
-                      round
-                      src={
-                        deliveryman.url
-                          ? deliveryman.url
-                          : 'https://api.adorable.io/avatars/116/abott@adorable.png '
-                      }
-                      size={25}
-                      textSizeRatio={2}
-                    />
-                  </td>
-                  <td>{deliveryman.name}</td>
-                  <td>{deliveryman.email}</td>
-                  <td>
-                    {/* Não consegui fazer o clic então criei 3 botões separados  */}
-                    <Link
-                      to={{
-                        pathname: `/editarentregador/${deliveryman.id}`,
-                        state: {
-                          deliveryman,
-                        },
-                      }}
-                    >
-                      <button className="icons">
-                        <MdModeEdit color="#1E90FF" />
-                      </button>
-                    </Link>
-                    <Link>
-                      <button className="icons">
-                        <MdDelete color="#B22222" />
-                      </button>
-                    </Link>
-                  </td>
+          {deliverymans.length > 0 ? (
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Foto</th>
+                  <th>Nome</th>
+                  <th>Email</th>
+                  <th>Ações</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-          {/* ) : (
+              </thead>
+              <tbody>
+                {deliverymans.map(deliveryman => (
+                  <tr key={deliveryman.id}>
+                    <td>#{deliveryman.id}</td>
+                    <td>
+                      <Avatar
+                        round
+                        src={
+                          deliveryman.url
+                            ? deliveryman.url
+                            : 'https://api.adorable.io/avatars/50/abott@adorable.png'
+                        }
+                        size={25}
+                        textSizeRatio={2}
+                      />
+                    </td>
+                    <td>{deliveryman.name}</td>
+                    <td>{deliveryman.email}</td>
+                    <td>
+                      {/* Não consegui fazer o clic então criei 3 botões separados  */}
+                      <Link
+                        to={{
+                          pathname: `/editarentregador/${deliveryman.id}`,
+                          state: {
+                            deliveryman,
+                          },
+                        }}
+                      >
+                        <button className="icons">
+                          <MdModeEdit color="#1E90FF" />
+                        </button>
+                      </Link>
+                      <Link
+                        onClick={() => confirmDeliverymanDelete(deliveryman.id)}
+                      >
+                        <button className="icons">
+                          <MdDelete color="#B22222" />
+                        </button>
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
             <h3>não há nada por aqui :/</h3>
-          )} */}
+          )}
         </ContentTable>
       </Content>
     </Container>
