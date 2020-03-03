b83b63d3036423e71d320d5753ec9a8bd9ce840f
Armando Joergensen <armando-a-j@hotmail.com>
2020-03-03 00:35:46 -0300

    NewDelivery
    Adicioando Deliveryman

---
+++ web/src/pages/Deliveryman/New/index.js
@@ -0,0 +1,63 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/button-has-type */
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import { MdDone, MdChevronLeft } from 'react-icons/md';
+import { toast } from 'react-toastify';
+import { Container, Content, StudentsContent } from './styles';
+import Header from '~/components/Header';
+import api from '~/services/api';
+import history from '~/services/history';
+
+import AvatarInput from '../AvatarInput';
+
+export default function New() {
+  async function handleSubmit({ avatar_id, email, name }) {
+    try {
+      const response = await api.post('deliverymans', {
+        name,
+        email,
+        avatar_id,
+      });
+      if (response) {
+        toast.success('Registro salvo!');
+        history.push('/entregador');
+      }
+    } catch (err) {
+      toast.error(err.response.data.ERRO);
+    }
+  }
+  return (
+    <Container>
+      <Header />
+      <Form onSubmit={handleSubmit}>
+        <Content>
+          <nav>
+            <h2>Cadastro de Encomendas</h2>
+            <div>
+              <Link to="/entregador">
+                <button>
+                  <MdChevronLeft size={19} />
+                  &nbsp;Voltar
+                </button>
+              </Link>
+              <button type="submit" className="last">
+                <MdDone size={19} />
+                &nbsp;Salvar
+              </button>
+            </div>
+          </nav>
+          <StudentsContent>
+            <AvatarInput name="avatar" required />
+            <label> Nome</label>
+            <Input name="name" placeholder="Digite o nome " required />
+            <label> Email</label>
+            <Input name="email" placeholder="Digite o email" required />
+          </StudentsContent>
+        </Content>
+      </Form>
+    </Container>
+  );
+}
