d50806a8037616d1c169fea61cb406b125053331
Armando Joergensen <armando-a-j@hotmail.com>
2020-03-13 23:58:52 -0300

    Config

--- appfastfeet/src/index.js
+++ appfastfeet/src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import '~/config/ReactotronConfig';
 
-// import { Container } from './styles';
+import Routes from '~/routes';
 
 export default function App() {
-  return (
-    <View>
-      <Text>Testando</Text>
-    </View>
-  );
+  return <Routes />;
 }
