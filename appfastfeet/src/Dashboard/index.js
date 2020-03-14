05c239c2b2a4c5c528f799838aa52fd6634f21bb
Armando Joergensen <armando-a-j@hotmail.com>
2020-03-13 23:58:27 -0300

    Dashboard

--- appfastfeet/src/Dashboard/index.js
+++ appfastfeet/src/Dashboard/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
 
-// import { Container } from './styles';
+import { Image } from 'react-native';
+
+import { Container, Input, Button, Text } from './styles';
+import Logo from '~/assets/img/logo.png';
 
 export default function Dashboard() {
-  return <View />;
+  const [userIds, setUserIds] = useState('');
+
+  return (
+    <Container>
+      <Image source={Logo} tintColor="rgb(255, 255, 255)" />
+      <Input
+        placeholder="Informe seu ID de cadastro"
+        keyboardType="numeric"
+        returnKeyType="send"
+        onChangeText={text => setUserIds({ setUserIds: text })}
+      />
+      <Button>
+        <Text>Entrar no sistema</Text>
+      </Button>
+    </Container>
+  );
 }
