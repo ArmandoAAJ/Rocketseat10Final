65ce9657b2c0d34a6a84f26a50e56a22f3863824
Armando Joergensen <armando-a-j@hotmail.com>
2020-03-15 18:49:44 -0300

    Update Routes

--- appfastfeet/src/routes.js
+++ appfastfeet/src/routes.js
@@ -1,9 +1,31 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-import Dashboard from '~/Dashboard';
+import Sign from '~/pages/Sign';
+import Dashboard from '~/pages/Dashboard';
 
-export default createAppContainer(
-  createSwitchNavigator({
-    Dashboard,
-  })
-);
+export default (signedIn = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          Sign,
+        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+          },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#7D40E7',
+              inactiveTintColor: 'rgba(255,255,255, 0.6)',
+            },
+          }
+        ),
+      },
+      {
+        initialRouteName: signedIn ? 'App' : 'Sign',
+      }
+    )
+  );
