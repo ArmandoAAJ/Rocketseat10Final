242af491c1bfdea813013434ba728027aac7d77e
Armando Joergensen <armando-a-j@hotmail.com>
2020-03-15 18:47:33 -0300

    Configs

--- appfastfeet/src/config/ReactotronConfig.js
+++ appfastfeet/src/config/ReactotronConfig.js
@@ -1,11 +1,13 @@
 import Reactotron from 'reactotron-react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
-  const tron = Reactotron.configure()
+  const tron = Reactotron.configure({ host: 'localhost' })
+    .setAsyncStorageHandler(AsyncStorage)
     .useReactNative()
     .connect();
 
-  console.tron = tron;
-
   tron.clear();
+
+  console.tron = tron;
 }
