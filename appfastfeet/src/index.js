2236cd395286f7511ec1ae49a92f9a218593f40a
Armando Joergensen <armando-a-j@hotmail.com>
2020-03-15 18:49:33 -0300

    Configs
    Tentei fazer a parte de entrar direto pelo async porem está funcionando só n segunda vez que renderiza

--- appfastfeet/src/index.js
+++ appfastfeet/src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '~/config/ReactotronConfig';
+import AsyncStorage from '@react-native-community/async-storage';
 
-import Routes from '~/routes';
+import createRouter from '~/routes';
 
 export default function App() {
+  const [signedIn, setSignedIn] = useState();
+
+  useEffect(() => {
+    async function teste() {
+      const logedIn = await AsyncStorage.getItem('logado');
+      setSignedIn(JSON.parse(logedIn));
+    }
+    teste();
+  }, [signedIn, setSignedIn]);
+
+  const Routes = createRouter(signedIn);
+
   return <Routes />;
 }
