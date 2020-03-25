9380b905119d162c164eb42745a0eb4b0bdf4113
Armando Joergensen <armando-a-j@hotmail.com>
2020-03-25 10:52:30 -0300

    Update

--- appfastfeet/src/pages/ViewProblem/index.js
+++ appfastfeet/src/pages/ViewProblem/index.js
@@ -15,13 +15,14 @@ import {
 
 export default function ViewProblem({ route }) {
   const [problems, setProblems] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const { order } = route.params;
 
   useEffect(() => {
     async function loadProblem() {
       try {
         const response = await api.get(`/problems/${order.id}`);
+
         const data = response.data.map(p => ({
           ...p,
           dateFormatted: format(parseISO(p.createdAt), 'dd/MM/yyyy', {
@@ -38,14 +39,13 @@ export default function ViewProblem({ route }) {
     loadProblem();
   }, [order]);
 
-  console.tron.log(loading);
   return (
     <>
       <StatusBar backgroundColor="rgb(125, 64, 231)" barStyle="light-content" />
       <Container>
         <Header>
           <Box>
-            {loading ? (
+            {!loading ? (
               <ActivityIndicator
                 size="large"
                 color="rgb(125, 64, 231)"
